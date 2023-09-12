7e4a8a27955a3e3c52c70f1b3f5d41012035c0a9
kv-codes <lekevin98@gmail.com>
2023-09-12 17:00:11 -0500

    Reset button

--- app.js
+++ app.js
@@ -2,4 +2,13 @@ function greetMe(){
     let name = document.getElementById("name").value;
     let greeting = "Hi there, " + name + ", have a wonderful day!";
     document.getElementById("output").textContent=greeting;
+
 };
+function Reset(){
+    
+    document.getElementById("form_id").reset();
+    let element = document.getElementById("output");
+    element.replaceChildren();
+
+    //document.getElementById("form").reset();
+}
